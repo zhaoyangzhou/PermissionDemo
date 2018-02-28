c6ede8bc5902477d64ea2cee5d4f53dc221cbeab
zhaoyang <zhaoyangzhou@users.noreply.github.com>
2018-02-28 08:51:27 +0800

    Update PermissionAspectjImpl.java

--- app/src/main/java/com/example/app/aop/PermissionAspectjImpl.java
+++ app/src/main/java/com/example/app/aop/PermissionAspectjImpl.java
@@ -7,7 +7,7 @@ import com.zzy.learn.aspectj.util.ActivityManager;
 
 import org.aspectj.lang.annotation.Aspect;
 
-import java.lang.ref.WeakReference;
+import java.lang.ref.SoftReference;
 
 /**
  * Package: com.example.app.aop
@@ -22,10 +22,10 @@ public class PermissionAspectjImpl extends PermissionAspectj {
     /**
      * Method: getCurrentActivity
      * Description: 获取当前显示的Activity引用
-     * @return  WeakReference<Activity>
+     * @return  SoftReference<Activity>
      */
     @Override
-    public WeakReference<Activity> getCurrentActivity() {
+    public SoftReference<Activity> getCurrentActivity() {
         return ActivityManager.getInstance().getCurrentActivity();
     }
 }
